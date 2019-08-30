a1d29c4c85556506c4097cb9ea0c75c9b34ee5ff
ChrisEng0212 <cjx02121981@gmail.com>
2019-08-31 07:44:40 +0800

    ConfigDB

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ import os
 
 
 class Config: 
+    
     SECRET_KEY = '145d4a32162599ef6f426668b77f2d9a' #This will protect the app against cookies. We need it for using forms, correctly. 
     #SQLALCHEMY_DATABASE_URI = 'postgres://apomnzwugaszpu:d0dd4b8a4a0eb4afb54af960f6d6fbdf0c251344cd62c0aff157808a62222382@ec2-54-83-201-84.compute-1.amazonaws.com:5432/d40v3i8lgf43pl'
     MAIL_SERVER = 'smtp.gmail.com'
