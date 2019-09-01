00803a594e4bab7c9d71028c912b596fd71e0825
ChrisEng0212 <cjx02121981@gmail.com>
2019-09-01 19:22:46 +0800

    Flask_Mail_Update

--- app.py
+++ app.py
@@ -16,13 +16,16 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login' # if user isn't logged in it will redirect to login page
 login_manager.login_message_category = 'info'
 
-app.config.update(dict(    
-    MAIL_SERVER = 'smtp.googlemail.com',
+app.config.update(dict(
+    MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = 587,
-    MAIL_USE_TLS = 1,
+    MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
     MAIL_USERNAME = 'chrisflask0212',
-    MAIL_PASSWORD = 'flask0212',
+    MAIL_PASSWORD = 'flask0212',    
+    MAIL_SUPPRESS_SEND = False,
+    MAIL_DEBUG = True,
+    TESTING = False
 ))
 
 mail = Mail(app)
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import json
 import os
 from os import environ
+try:
+    from aws import Settings
+    DATABASE = Settings.SQLALCHEMY_DATABASE_URI
+except:
+    pass
 
 
 class BaseConfig: 
@@ -8,11 +13,10 @@ class BaseConfig:
     
     try:  
         SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']         
-        DEBUG = False
+        DEBUG = os.environ['DEBUG'] 
         print ('Config_Success')        
-    except:
-        #TravelEng DB
-        SQLALCHEMY_DATABASE_URI = 'postgres://apomnzwugaszpu:d0dd4b8a4a0eb4afb54af960f6d6fbdf0c251344cd62c0aff157808a62222382@ec2-54-83-201-84.compute-1.amazonaws.com:5432/d40v3i8lgf43pl'
+    except:        
+        SQLALCHEMY_DATABASE_URI = DATABASE
         MAIL_USERNAME = 'chrisflask0212@gmail.com'
         MAIL_PASSWORD = 'flask0212'
         DEBUG = True 
